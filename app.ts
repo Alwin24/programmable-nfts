cdd5aa8ed7ae7bb84e904de788a36f62d19a21b9
alwin <alwin24012002@gmail.com>
2023-02-24 00:19:30 +0530

    .

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
 import { Connection, Keypair, PublicKey, clusterApiUrl } from '@solana/web3.js'
-import { Metaplex, keypairIdentity, bundlrStorage, toMetaplexFile, toBigNumber } from '@metaplex-foundation/js'
-import { TokenStandard } from '@metaplex-foundation/mpl-token-metadata'
+import {
+	Metaplex,
+	keypairIdentity,
+	bundlrStorage,
+	toMetaplexFile,
+	toBigNumber,
+	walletAdapterIdentity,
+	transferNftBuilder,
+} from '@metaplex-foundation/js'
+import { TokenStandard, createTransferInstruction } from '@metaplex-foundation/mpl-token-metadata'
 import { readFileSync } from 'fs'
 
 const QUICKNODE_RPC = clusterApiUrl('devnet')
@@ -9,7 +17,8 @@ const WALLET = Keypair.fromSecretKey(
 	new Uint8Array(JSON.parse(readFileSync('/home/alwin/second-wallet/second-keypair.json', 'utf8')))
 )
 
-const metaplex = Metaplex.make(SOLANA_CONNECTION).use(keypairIdentity(WALLET))
+const metaplex = Metaplex.make(SOLANA_CONNECTION)
+// const metaplex = Metaplex.make(SOLANA_CONNECTION).use(keypairIdentity(WALLET))
 // .use(
 // 	bundlrStorage({
 // 		address: 'https://devnet.bundlr.network',
@@ -33,31 +42,22 @@ async function mintProgrammableNft(
 	symbol: string,
 	creators: { address: PublicKey; share: number }[]
 ) {
-	console.log(`Minting pNFT`)
 	try {
-		// let mintTxnBuilder = await metaplex
-		// 	.nfts()
-		// 	.builders()
-		// 	.create({
-		// 		uri: metadataUri,
-		// 		name: name,
-		// 		sellerFeeBasisPoints: sellerFee,
-		// 		symbol: symbol,
-		// 		creators: creators,
-		// 		isMutable: true,
-		// 		isCollection: false,
-		// 		maxSupply: toBigNumber(1),
-		// 		tokenStandard: TokenStandard.ProgrammableNonFungible,
-		// 		ruleSet: null,
-		// 	})
-
-		// let { signature, confirmResponse } = await metaplex.rpc().sendAndConfirmTransaction(mintTxnBuilder)
-		// if (confirmResponse.value.err) {
-		// 	throw new Error('failed to confirm transaction')
-		// }
-
-		// console.log(`Success!🎉`)
-		// console.log(`Tx: https://explorer.solana.com/tx/${signature}?cluster=devnet`)
+		let mintTxnBuilder = await metaplex
+			.nfts()
+			.builders()
+			.create({
+				uri: metadataUri,
+				name: name,
+				sellerFeeBasisPoints: sellerFee,
+				symbol: symbol,
+				creators: creators,
+				isMutable: true,
+				isCollection: false,
+				maxSupply: toBigNumber(1),
+				tokenStandard: TokenStandard.ProgrammableNonFungible,
+				ruleSet: null,
+			})
 
 		let transferTxnBuilder = metaplex
 			.nfts()
@@ -70,11 +70,43 @@ async function mintProgrammableNft(
 				toOwner: new PublicKey('BcSZEL6ryTaLS7unjzPuSLNgi1VVj6yxGKHJMMd44i5Z'),
 			})
 
-		let { signature, confirmResponse } = await metaplex.rpc().sendAndConfirmTransaction(transferTxnBuilder)
+		console.log(
+			'Signers :',
+			transferTxnBuilder.getSigners().map((signer) => signer.publicKey.toBase58())
+		)
+
+		let delegateTxnBuilder = metaplex
+			.nfts()
+			.builders()
+			.delegate({
+				nftOrSft: {
+					tokenStandard: TokenStandard.ProgrammableNonFungible,
+					address: new PublicKey('Cf6SV5WiwsmAFU1ZLh1aD2MazyQBYpSDA2AVHxUHwGgS'),
+				},
+				delegate: {
+					delegate: new PublicKey('HtUaVzWiSNrrY2NSVKroE3883vnBfn8SMrLM2UxA2vDy'),
+					owner: WALLET.publicKey,
+					type: 'TransferV1',
+					data: {
+						amount: toBigNumber(1),
+					},
+				},
+			})
+
+		delegateTxnBuilder.add(
+			metaplex
+				.nfts()
+				.builders()
+				.freezeDelegatedNft({
+					mintAddress: new PublicKey('Cf6SV5WiwsmAFU1ZLh1aD2MazyQBYpSDA2AVHxUHwGgS'),
+					delegateAuthority: WALLET,
+				})
+		)
+
+		let { signature, confirmResponse } = await metaplex.rpc().sendAndConfirmTransaction(delegateTxnBuilder)
 		if (confirmResponse.value.err) {
 			throw new Error('failed to confirm transaction')
 		}
-
 		console.log(`Success!🎉`)
 		console.log(`Tx: https://explorer.solana.com/tx/${signature}?cluster=devnet`)
 	} catch (err) {
